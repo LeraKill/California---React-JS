16965f6cf1023be852a21b48fb65b3ba1238c929
LeraKill <kilinav25@gmail.com>
2022-09-27 14:14:40 +0400

    fixed flex elements and added adaptive to feature item

--- src/components/Categories/Categories.jsx
+++ src/components/Categories/Categories.jsx
@@ -16,10 +16,13 @@ const Categories = () => {
         />
         <div className="categories__body">
           <div className="categories__left-part">
-            <a href="/" className="categories__item__link">
-              <div className="categories__item categories__item-laptop">
+            <div className="categories__item categories__item-laptop">
+              <a
+                href="/"
+                className="categories__item__link categories__item-laptop-link"
+              >
                 <div className="categories__item__img categories__item-laptop__img _ibg">
-                  <img src={LaptopImg} alt="Laptop Image" />
+                  <img src={LaptopImg} alt="Laptop" />
                 </div>
                 <div className="categories__item__info categories__item-laptop__info">
                   <p className="categories__item-text categories__item-laptop__text">
@@ -29,12 +32,16 @@ const Categories = () => {
                     True Laptop Solution
                   </h3>
                 </div>
-              </div>
-            </a>
-            <a href="/" className="categories__item__link">
-              <div className="categories__item categories__item-watches">
+              </a>
+            </div>
+
+            <div className="categories__item categories__item-watches">
+              <a
+                href="/"
+                className="categories__item__link categories__item-watches-link"
+              >
                 <div className="categories__item__img categories__item-watches__img _ibg">
-                  <img src={WatchesImg} alt="Watches Image" />
+                  <img src={WatchesImg} alt="Watches" />
                 </div>
                 <div className="categories__item__info categories__item-watches__info">
                   <p className="categories__item-text categories__item-watches__text">
@@ -44,14 +51,17 @@ const Categories = () => {
                     Not just stylisht
                   </h3>
                 </div>
-              </div>
-            </a>
+              </a>
+            </div>
           </div>
           <div className="categories__right-part">
-            <a href="/" className="categories__item__link">
-              <div className="categories__item categories__item-phones">
+            <div className="categories__item categories__item-phones">
+              <a
+                href="/"
+                className="categories__item__link categories__item-phones-link"
+              >
                 <div className="categories__item__img categories__item-phones__img _ibg">
-                  <img src={PhoneImg} alt="Phone Image" />
+                  <img src={PhoneImg} alt="Phone" />
                 </div>
                 <div className="categories__item__info categories__item-phones__info">
                   <p className="categories__item-text categories__item-phones__text">
@@ -61,12 +71,16 @@ const Categories = () => {
                     Your day to day life
                   </h3>
                 </div>
-              </div>
-            </a>
-            <a href="/" className="categories__item__link">
-              <div className="categories__item categories__item-tablet">
+              </a>
+            </div>
+
+            <div className="categories__item categories__item-tablet">
+              <a
+                href="/"
+                className="categories__item__link categories__item-tablet-link"
+              >
                 <div className="categories__item__img categories__item-tablet__img _ibg">
-                  <img src={TabletImg} alt="Tablet Image" />
+                  <img src={TabletImg} alt="Tablet" />
                 </div>
                 <div className="categories__item__info categories__item-tablet__info">
                   <p className="categories__item-text categories__item-tablet__text">
@@ -76,8 +90,8 @@ const Categories = () => {
                     Empower your work
                   </h3>
                 </div>
-              </div>
-            </a>
+              </a>
+            </div>
           </div>
         </div>
       </div>
