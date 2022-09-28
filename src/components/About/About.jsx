6a0936f3bca3aa0a2538c059da551a11e97a961d
LeraKill <kilinav25@gmail.com>
2022-09-28 18:41:29 +0400

    map as a separate component

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./style.scss";
 import FeatureItem from "../Any/FeatureItem/FeatureItem";
 import AboutImg from "../../assets/images/about/about.png";
+import AboutMap from "./AboutMap/AboutMap";
 
 const About = () => {
   return (
@@ -42,19 +43,7 @@ const About = () => {
             </div>
           </div>
         </div>
-        <div className="about__map">
-          <iframe
-            src={
-              "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d992475.578733176!2d-117.844589498679!3d34.055559732843804!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80dca79ab6906791%3A0x4f1d4890ceec6a18!2zQ2VkYXIgQ3QsIE1vcmVubyBWYWxsZXksIENBIDkyNTU1LCDQodCo0JA!5e0!3m2!1sru!2sge!4v1664374687264!5m2!1sru!2sge"
-            }
-            width={"100%"}
-            height={311}
-            style={{ border: 0 }}
-            allowFullScreen={""}
-            loading={"lazy"}
-            referrerPolicy={"no-referrer-when-downgrade"}
-          ></iframe>
-        </div>
+        <AboutMap />
       </div>
     </section>
   );
